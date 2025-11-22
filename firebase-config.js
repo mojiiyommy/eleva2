7a9dd2713c987c04ccd353706c20443d9866fa01
mojiiyommy <jujrm020206@gmail.com>
2025-11-22 01:18:39 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+firebase-config.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAG3CVkdhtA4fze4F92u5sl4wM5e5cKIMo",
+  authDomain: "eleva-8c998.firebaseapp.com",
+  projectId: "eleva-8c998",
+  storageBucket: "eleva-8c998.firebasestorage.app",
+  messagingSenderId: "71808646586",
+  appId: "1:71808646586:web:0ab5d2d4f892cc7c7a7183"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
